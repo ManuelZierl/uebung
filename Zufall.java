6356b829d168d40e68045d8677898b6a42ca3810
ru76sin <manuel.zierl@campus.lmu.de>
2020-07-13 12:38:03 +0200

    removed

--- Zufall.java
+++ Zufall.java
@@ -16,40 +16,26 @@ public class Zufall
     
 
     
-    
+    /*
     public int zufall_int(int von, int bis)
     {
         // todo: soll einen zufall wert von-bis zurueckgeben
-        int range = bis - von;
-        int r = rand.nextInt(range);
-        return von + r;
-    }
+    }*/
     
-
+    /*
     public boolean zufall_bool()
     {
         // todo: soll zufaellig true oder false zurueckgeben
-        double double_random=rand.nextDouble();
-        if (double_random > 0.5)
-        {
-            return true;
         
-        }
-        else {
-            return false;
-        }
-        
-    }
+    }*/
     
     
-    
+    /*
     public <E> E zufall_element(E[] liste)
     {
         // todo: soll eine zufaelliges element aus der liste zurückgeben
-        int z = zufall_int(0, liste.length);
-        return liste[z];
         
-    }
+    }*/
     
     
 }
